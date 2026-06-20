9d831eca7f014f197f81f1a321b46d0ab2f86f9e
Ryan LAU From.TP <ryan_lau_from.tp@moe.gov.sg>
2026-06-20 17:00:58 +0800

    black

--- main.py
+++ main.py
@@ -8,6 +8,22 @@ from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
 from tavily import TavilyClient
 from langchain_tavily import TavilySearch
+from typing import List
+
+from pydantic import BaseModel, Field
+
+class Source(BaseModel):
+    """
+    Schema for a source used by the agent.
+    """
+    url: str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """
+    Schema for the agent response with the answer and sources
+    """
+    answer: str = Field(description="The agent's answer to the query")
+    sources: List[Source] = Field(default_factory=list, description="List of sources used to generate the answer")
 
 # tavily = TavilyClient()
 
@@ -25,12 +41,19 @@ from langchain_tavily import TavilySearch
 
 llm = ChatOllama(temperature=0, model="gemma4")
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 def main():
     print("Hello from langchain-course!")
-    result = agent.invoke({"messages": HumanMessage(content="Search for 3 jobs posters for an ai engineer using langchain and living in Singapore.")})
-    print(result)
+    result = agent.invoke(
+        {
+            "messages": HumanMessage(
+                content="Search for 3 jobs posters for an ai engineer using langchain and living in Singapore."
+            )
+        }
+    )
+    print(result['structured_response'])
+
 
 if __name__ == "__main__":
     main()
