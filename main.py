0987e0cd7d25cbe70e01f550c6549d1c3f95bb36
Ryan LAU From.TP <ryan_lau_from.tp@moe.gov.sg>
2026-06-20 16:16:19 +0800

    travily agent set up

---
+++ main.py
@@ -0,0 +1,36 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
+from tavily import TavilyClient
+from langchain_tavily import TavilySearch
+
+# tavily = TavilyClient()
+
+# @tool
+# def search(query: str) -> str:
+#     """
+#     Tool that searches over internet
+#     Args:
+#         query: The query to search for
+#     Returns:
+#         The search result
+#     """
+#     print(f"Searching for {query}")
+#     return tavily.search(query=query)
+
+llm = ChatOllama(temperature=0, model="gemma4")
+tools = [TavilySearch()]
+agent = create_agent(model=llm, tools=tools)
+
+def main():
+    print("Hello from langchain-course!")
+    result = agent.invoke({"messages": HumanMessage(content="Search for 3 jobs posters for an ai engineer using langchain and living in Singapore.")})
+    print(result)
+
+if __name__ == "__main__":
+    main()
